7f8c06100bd86be06d72c6dba2536b196dff1e08
MatiasMaldonadoLera <matiasmal_94@hotmail.com>
2022-10-14 11:36:16 -0300

    primer commit

---
+++ PeliculasAPI/Controllers/GenerosController.cs
@@ -0,0 +1,73 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using PeliculasAPI.DTOs;
+using PeliculasAPI.Entidades;
+
+namespace PeliculasAPI.Controllers
+{
+    [ApiController]
+    [Route("api/generos")]
+    public class GenerosController:ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IMapper mapper;
+
+        public GenerosController(ApplicationDbContext context, IMapper mapper)
+        {
+            this._context = context;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<GeneroDTO>>> Get()
+        {
+            var entidades = await _context.Generos.ToListAsync();
+            var dtos = mapper.Map<List<GeneroDTO>>(entidades);
+            return dtos;
+        }
+
+        [HttpGet ("{id:int}", Name = "ObtenerGenero")]
+        public async Task<ActionResult<GeneroDTO>> Get(int id)
+        {
+            var entidad = await _context.Generos.FirstOrDefaultAsync(x=> x.Id == id);
+            if(entidad == null)
+            {
+                return NotFound("No se encontro el genero");
+            }
+            var dtos = mapper.Map<GeneroDTO>(entidad);
+            return dtos;
+        }
+        [HttpPost]
+        public async Task<ActionResult>Post([FromBody] GeneroCreacionDTO generoCreacion) 
+        { 
+            var entidad = mapper.Map<Genero>(generoCreacion);
+            _context.Add(entidad);
+            await _context.SaveChangesAsync();
+            var generoDto= mapper.Map<GeneroDTO>(entidad);
+
+            return new CreatedAtRouteResult("ObtenerGenero", new { id = generoDto.Id }, generoDto);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> Put (int id, [FromBody] GeneroCreacionDTO generoCreacionDTO)
+        {
+            var entidad = mapper.Map<Genero>(generoCreacionDTO);
+            entidad.Id = id;
+            _context.Entry(entidad).State= EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            var entidad = await _context.Generos.AnyAsync(x => x.Id == id);
+            if (!entidad)
+            {
+                return NotFound();
+            }
+            _context.Remove(new Genero() { Id = id });
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
---
+++ PeliculasAPI/Startup.cs
@@ -0,0 +1,64 @@
+﻿
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System.Reflection;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+[assembly: ApiConventionType(typeof(DefaultApiConventions))]
+namespace PeliculasAPI
+{
+    public class Startup
+    {
+
+        public Startup(IConfiguration configuration)
+        {
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(Startup));
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllers(opciones =>
+            {
+            }
+            );
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILogger <Startup> logger )
+        {
+
+
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseCors();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(end =>
+            {
+                end.MapControllers();
+            });
+            //app.MapControllers();
+
+            //app.Run();
+
+        }
+
+    }
+}
